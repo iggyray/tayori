66b683e74024665cd8dff1401be9c0979a5949fa
iggy <iggynelo@gmail.com>
2023-09-16 16:31:59 +0900

    added flush to test to ensure that component fully renders before testing

--- src/components/__tests__/EssayComponent.spec.ts
+++ src/components/__tests__/EssayComponent.spec.ts
@@ -1,9 +1,9 @@
-import { mount } from '@vue/test-utils'
+import { flushPromises, mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
 import EssayComponent from '../EssayComponent.vue'
 
 describe('EssayComponent.vue tests', () => {
-  it('should render props correctly', () => {
+  it('should render props correctly', async () => {
     // Arrange
     const title = 'hello'
     const body = 'she sells sea shells'
@@ -17,6 +17,7 @@ describe('EssayComponent.vue tests', () => {
     const wrapper = mount(EssayComponent, {
       props: defaultProps
     })
+    await flushPromises()
 
     // Assert
     expect(wrapper.find('[data-cy=essay-component__title]').text()).toStrictEqual(title)
