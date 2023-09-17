1783b54a147f404ca99f923aac2ad85ade7cefe2
iggy <iggynelo@gmail.com>
2023-09-17 14:57:04 +0900

    implemented sidebar component and added page header

---
+++ src/components/SideBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="side-bar">
+    <nav>
+      <p><router-link to="/">about</router-link></p>
+      <p><router-link to="/essays">essays</router-link></p>
+    </nav>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { RouterLink } from 'vue-router'
+
+export default defineComponent({
+  components: { RouterLink },
+  setup() {}
+})
+</script>
+<style scoped>
+.side-bar {
+  height: 100px;
+  font-size: 18px;
+  color: black;
+  text-align: right;
+}
+ul {
+  list-style-type: none;
+}
+a {
+  padding: 2px 10px;
+}
+p {
+  margin-bottom: -10px;
+}
+</style>
