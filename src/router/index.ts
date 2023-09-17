9b3b0e5fdceeb36674d6389359224f202f86de20
iggy <iggynelo@gmail.com>
2023-09-17 15:40:57 +0900

    implemented about view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ContentView from '../views/ContentView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: AboutView
+    },
     {
       path: '/essays',
       name: 'content',
