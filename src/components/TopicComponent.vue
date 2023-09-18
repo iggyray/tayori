24e87fa61cda6887258dc6c00f41223d7c9a2286
iggy <iggynelo@gmail.com>
2023-09-18 17:38:08 +0900

    implemented topic component and unit test

---
+++ src/components/TopicComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <a @click="redirect">
+    <div class="topic-component">
+      <img :src="topic.imageLink" data-cy="topic-component__image" />
+      <h4 data-cy="topic-component__header">{{ topic.header }}</h4>
+    </div>
+  </a>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { useRouter } from 'vue-router'
+import type { Topic } from '../views/ContentView.vue'
+
+export default defineComponent({
+  props: {
+    topic: { type: Object as PropType<Topic>, required: true }
+  },
+  setup(props) {
+    const router = useRouter()
+    const redirect = () => {
+      router.push('essays/' + props.topic.id)
+    }
+
+    return {
+      redirect
+    }
+  }
+})
+</script>
+<style scoped>
+.topic-component {
+  margin-bottom: 30px;
+}
+h4 {
+  width: 250px;
+  margin-top: 5px;
+}
+img {
+  width: 250px;
+  height: 168px;
+}
+a:hover {
+  cursor: pointer;
+}
+</style>
---
+++ src/components/__tests__/TopicComponent.spec.ts
@@ -0,0 +1,36 @@
+import { flushPromises, mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import { createMemoryHistory, createRouter } from 'vue-router'
+import TopicComponent from '../TopicComponent.vue'
+
+describe('TopicComponent tests', () => {
+  const defaultProps = {
+    topic: {
+      id: 'curry-rice',
+      header: 'topic-header',
+      imageLink: 'image-link.jpg'
+    }
+  }
+
+  const mockRouter = createRouter({
+    history: createMemoryHistory(),
+    routes: []
+  })
+
+  it('should render props correctly', async () => {
+    // Arrange
+    const wrapper = mount(TopicComponent, {
+      props: defaultProps,
+      global: {
+        plugins: [mockRouter]
+      }
+    })
+    await flushPromises()
+
+    // Assert
+    expect(wrapper.find('[data-cy=topic-component__header]').text()).toStrictEqual('topic-header')
+    expect(wrapper.find('[data-cy=topic-component__image]').attributes('src')).toStrictEqual(
+      'image-link.jpg'
+    )
+  })
+})
